6339d74fae058fad39acf0f3d1c293d8d35acc8f
MERWAN SI_TAYEB <merwan.st@gmail.com>
2025-01-07 11:56:16 +0100

    ended footer component and some frontend adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { ButtonComponent } from './components/button/button.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavbarComponent, ButtonComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, ButtonComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
