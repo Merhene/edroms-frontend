58888eb6ebd2d139a6c50a09d038796130801f57
MERWAN SI_TAYEB <merwan.st@gmail.com>
2025-01-07 12:13:35 +0100

    updated homepage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from "./components/navbar/navbar.component";
-import { ButtonComponent } from './components/button/button.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavbarComponent, ButtonComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
