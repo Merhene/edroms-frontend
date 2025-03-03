c345d8132f53d9ebf41386235cd1b6643c61682f
MERWAN SI_TAYEB <merwan.st@gmail.com>
2025-03-03 10:52:44 +0100

    added calendar for weekguard component

--- src/app/pages/weekguardpage/weekguardpage.component.ts
+++ src/app/pages/weekguardpage/weekguardpage.component.ts
@@ -1,13 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 @Component({
   selector: 'app-weekguardpage',
   standalone: true,
-  imports: [ RouterLink ],
+  imports: [ RouterLink, CommonModule, FullCalendarModule ],
   templateUrl: './weekguardpage.component.html',
   styleUrl: './weekguardpage.component.css'
 })
 export class WeekguardpageComponent {
   router: Router = inject(Router);
+  calendarOptions: CalendarOptions = {
+    initialView: 'timeGridDay',
+    plugins: [timeGridPlugin, listPlugin, dayGridPlugin],
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek'
+    }
+  };
 }
